1228323f05e1cf673771b01524074895f1e069df
eudes.neto <eudes.neto@ambevtech.com.br>
2022-02-21 11:53:01 -0300

    Curso Finalizado

---
+++ src/Components/Extrato/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Box, Botao } from "../../Components/UI";
+import { extratoLista } from "../../info";
+import Itens from "../Itens";
+
+const Extrato = () => {
+  return (
+    <Box>
+      {extratoLista.updates.map(({ id, type, value, date, from }) => {
+        return (
+          <Itens key={id} type={type} from={from} value={value} date={date} />
+        );
+      })}
+      <Botao>Ver mais</Botao>
+    </Box>
+  );
+};
+
+export default Extrato;
